dec30082174824fd296a45b4e59e982ba11bc826
SaidMendoza <112911042+SaidMendoza@users.noreply.github.com>
2023-05-02 10:32:28 -0600

    Tarea IGrafica

---
+++ InterfazGrafica/src/Main.java
@@ -0,0 +1,10 @@
+import IU.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        Principal ventana = new Principal("Hola mundo AWT");
+
+    }
+}
